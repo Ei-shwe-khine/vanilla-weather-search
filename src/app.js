9069ea5ef88b64ee361488bc0f008a13933c0abc
Ei-shwe-khine <69576300+Ei-shwe-khine@users.noreply.github.com>
2023-03-16 22:45:04 +0630

    showing the current temperature

---
+++ src/app.js
@@ -0,0 +1,26 @@
+function displayTemperature(response) {
+    
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.name;
+
+    let description = document.querySelector("#description");
+    description.innerHTML = response.data.weather[0].description;
+
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.main.humidity;
+
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML =Math.round(response.data.wind.speed);
+
+
+}
+
+
+
+let apiKey = "6655e6104636a644c9bc754d824a695d";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
