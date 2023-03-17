2ccd3662ff8a3401bcd07d7b0755241bc25f1792
Ei-shwe-khine <69576300+Ei-shwe-khine@users.noreply.github.com>
2023-03-17 14:54:25 +0630

    added the search engine

--- src/app.js
+++ src/app.js
@@ -47,9 +47,22 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-
-
+function search(city) {
+    
 let apiKey = "6655e6104636a644c9bc754d824a695d";
-let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
 axios.get(apiUrl).then(displayTemperature);
+
+
+}
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
