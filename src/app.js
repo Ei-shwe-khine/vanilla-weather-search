522866cc96b5a5ad7ebf9bd3a17b0d7d5c02bff5
Ei-shwe-khine <69576300+Ei-shwe-khine@users.noreply.github.com>
2023-03-17 18:27:59 +0630

    Implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -45,6 +45,8 @@ function displayTemperature(response) {
         `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     
     iconElement.setAttribute("alt", response.data.weather[0].description);
+
+    celsiusTemperature = response.data.main.temp;
 }
 
 function search(city) {
@@ -62,7 +64,41 @@ function handleSubmit(event) {
     search(cityInputElement.value);
 }
 
-search("New York");
+function displayFahrenheitTemperature(event) {
+    
+    event.preventDefault();
+
+    let temperatureElement = document.querySelector("#temperature");
+
+    //remove the active class the clsius link
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+
+}
+let celsiusTemperature = null;
+
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+
+search("New York");
