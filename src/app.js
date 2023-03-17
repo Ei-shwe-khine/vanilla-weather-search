515b95ec7837dc31d1c4e628daa133e706c34662
Ei-shwe-khine <69576300+Ei-shwe-khine@users.noreply.github.com>
2023-03-17 11:12:32 +0630

    added relevant icon from api

--- src/app.js
+++ src/app.js
@@ -40,11 +40,16 @@ function displayTemperature(response) {
     let dateElement = document.querySelector("#date");
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute("src",
+        `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 
 
 let apiKey = "6655e6104636a644c9bc754d824a695d";
-let city = "Myanmar";
+let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
